05b24c8bb44d6bd323354d5ac8499487590226ea
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-04-06 15:47:56 +0500

    Create Lesson_3.py
    Lesson3 Homework

---
+++ Lesson_3.py
@@ -0,0 +1,31 @@
+# Homework
+# IT_PARK = ['Backend dastrulash', 'Frontend dasturlash', 'Android dastrulash', 'SMM', 'Grafik dizayn']
+# print("IT park kurslar ro'yxati:", IT_PARK)
+# del IT_PARK[1]
+# IT_PARK.append('Mobilografiya')
+# IT_PARK.append('IT English')
+# print("IT park kurslar ro'yxati:", IT_PARK)
+
+VariableType = []
+element = input("Type nomini kiriting:")
+VariableType.append(element)
+element = input("Type nomini kiriting:")
+VariableType.append(element)
+element = input("Type nomini kiriting:")
+VariableType.append(element)
+element = input("Type nomini kiriting:")
+VariableType.append(element)
+print(VariableType)
+n = int(input("O'zgartirmoqchi bo'lgan elemtn indexini kiriting:"))
+m = int(input("Yana qaysi elemtni o'zgatiramiz:"))
+print(VariableType[n].capitalize())
+print(VariableType[m].upper())
+# VariableType = []
+# n = int(input("Nechta element kiritasiz:"))
+# for i in range(n):
+#     element = input(f"{i + 1} - elementni kiriting:")
+#     VariableType.append(element)
+# print(VariableType)
+
+# print(VariableType[0].title())
+# print(VariableType[1].upper())
