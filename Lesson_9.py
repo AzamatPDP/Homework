615532b7fbfa15c4960976960ff98b997d1d6464
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-04-07 12:55:43 +0500

    Create Lesson_9.py
    Lesson9 homework

---
+++ Lesson_9.py
@@ -0,0 +1,31 @@
+# Homework
+# print("n dan m gacha 3 ga qoldiqsiz bo'linuvchi sonlar.")
+# n = int(input("n:"))
+# m = int(input("m:"))
+# while n < m:
+#     if n % 3 == 0:
+#         print(n,end=" ")
+#     n+=1
+
+# print("n butun sonini tublikga tekshiruvchi dastur.")
+# n = int(input("Ixtiyoriy butun son kiriting=>"))
+# i = 1
+# k = 0
+# while i <= n:
+#     if n % i == 0:
+#         k += 1
+#     if k < 2: break
+#     i += 1
+# if k == 2:
+#     print(f"{n} tub son")
+# else:
+#     print(f"{n} tub son emas")
+
+# print("2 ta sonning EKUBini hisoblovchi dastur.")
+# a = int(input("a sonini kiriting:"))
+# b = int(input("b sonini kiriting:"))
+# while a != b:
+#     if a > b: a-=b
+#     else: b-=a
+# print(f"EKUB:{a}")
+
