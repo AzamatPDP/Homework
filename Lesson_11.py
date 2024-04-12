d05e1bd579204ccdb3ec4725aea86a25555d1f12
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-04-12 19:44:30 +0500

    Update Lesson_11.py

--- Lesson_11.py
+++ Lesson_11.py
@@ -21,23 +21,34 @@
 # print(aylanaInfo)
 # print("Aylanada yuza bo'lmaydi!")
 
-def tub_sonlar(n, m):
-    """Berilgan n va m butun musbat sonlar orasidagi tub sonlar"""
-    tub = []
-    while n <= m:
-        i = n
-        j = 1
-        boluvchi = 0
-        while j <= i:
-            if i % j == 0:
-                boluvchi += 1
-            j += 1
-        if boluvchi == 2:
-            tub.append(i)
-        n += 1
-    return tub
-n = int(input("n=>"))
-m = int(input("m=>"))
-tublik = tub_sonlar(n, m)
-print(f"Tub sonlar: {tublik}")
+# def tub_sonlar(n, m):
+#     """Berilgan n va m butun musbat sonlar orasidagi tub sonlar"""
+#     tub = []
+#     while n <= m:
+#         i = n
+#         j = 1
+#         boluvchi = 0
+#         while j <= i:
+#             if i % j == 0:
+#                 boluvchi += 1
+#             j += 1
+#         if boluvchi == 2:
+#             tub.append(i)
+#         n += 1
+#     return tub
+# n = int(input("n=>"))
+# m = int(input("m=>"))
+# tublik = tub_sonlar(n, m)
+# print(f"Tub sonlar: {tublik}")
+
+def fibonachi(n):
+    fibo = [0, 1]
+    i = 2
+    while i < n:
+        fibo.append(fibo[-1] + fibo[-2])
+        i += 1
+    return fibo
+son = int(input("Nechta fibonachi sonlari kerak:"))
+print(fibonachi(son))
+
 
