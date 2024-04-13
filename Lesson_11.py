c048d8d327c00e554c49054307f1001499bd1225
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-04-13 14:06:49 +0500

    Update Lesson_11.py

--- Lesson_11.py
+++ Lesson_11.py
@@ -1,25 +1,20 @@
-# def aylana(radius, uzunlik, yuza, diametr):
-#     aylana_info = {
-#         'radius':radius,
-#         'uzunlik':uzunlik,
-#         'yuza':yuza,
-#         'diametr':diametr
-#     }
-#     return aylana_info
-#
-# aylanaInfo = []
-# while True:
-#     radius = float(input("Radiusini kiriting:"))
-#     uzunlik = 2 * 3.14 * radius
-#     yuza = 0
-#     diametr = 2 * radius
-#
-#     aylanaInfo.append(aylana(radius,uzunlik,yuza,diametr))
-#     javob = input("Yana aylana radius kiritasizmi? (yes/no): ")
-#     if javob == 'no':
-#         break
-# print(aylanaInfo)
-# print("Aylanada yuza bo'lmaydi!")
+def aylana(radius):
+    uzunlik = 2 * 3.14 * radius
+    yuza = 0
+    diametr = 2 * radius
+    
+    aylana_info = {
+        'radius':radius,
+        'uzunlik':uzunlik,
+        'yuza':yuza,
+        'diametr':diametr
+    }
+    return aylana_info
+
+radius = float(input("Radius kiriting:"))
+aylana_info = aylana(radius)
+print(aylana_info)
+
 
 # def tub_sonlar(n, m):
 #     """Berilgan n va m butun musbat sonlar orasidagi tub sonlar"""
@@ -51,13 +46,13 @@
 # son = int(input("Nechta fibonachi sonlari kerak:"))
 # print(fibonachi(son))
 
-def fibonachi(n):
-    fibo = [0, 1]
-    for i in range(2, n):
-        fibo.append(fibo[-1] + fibo[-2])
-    return fibo
-son = int(input("Nechta fibonachi sonlari kerak:"))
-print(fibonachi(son))
+# def fibonachi(n):
+#     fibo = [0, 1]
+#     for i in range(2, n):
+#         fibo.append(fibo[-1] + fibo[-2])
+#     return fibo
+# son = int(input("Nechta fibonachi sonlari kerak:"))
+# print(fibonachi(son))
 
 
 
