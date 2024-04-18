37c3009c4f703ad319a000d26e9080616bdcdf54
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-04-18 13:40:43 +0500

    Update Lesson_13.py

--- Lesson_13.py
+++ Lesson_13.py
@@ -3,12 +3,13 @@
 # musbat_numbers = list(filter(lambda num: num > 0,numbers))
 # print(musbat_numbers)
 
-numbers = list(range(1,6))
-teskari_list = lambda num : num[::-1]
-print(teskari_list(numbers))
+# numbers = list(range(1,6))
+# teskari_list = lambda num : num[::-1]
+# print(teskari_list(numbers))
 
-# numbers = list(range(-5,5))
-# juft_numbers = list(filter(lambda num : num % 2 == 0,numbers))
-# print(juft_numbers)
-# kvadrat = list(map(lambda num : num * num, juft_numbers))
-# print(kvadrat)
+numbers = list(range(-5,5))
+print(numbers)
+juft_numbers = list(filter(lambda num : num % 2 == 0,numbers))
+print(juft_numbers)
+kvadrat = list(map(lambda num : num * num, juft_numbers))
+print(kvadrat)
