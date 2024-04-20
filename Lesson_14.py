80bec122086562379b722d9f25d3844e5e547b38
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-04-20 14:08:09 +0500

    update

--- Lesson_14.py
+++ Lesson_14.py
@@ -14,9 +14,9 @@ class User:
     def get_email(self):
         return f"Email:{self.email}"
     def get_info(self):
-        print(f"Foydalanuvchi nomim '{self.username}', to'liq ismim '{self.toliqism}' va menga murojaat qilishingiz uchun => {self.email}")
+        print(f"Github username '{self.username}', to'liq ismim '{self.toliqism}' va menga murojaat qilishingiz uchun => {self.email}")
 
-user1 = User("azamat_developer","Azamat Jumabayev", 20, "azamatjumabayev8774@gmail.com")
+user1 = User("AzamatPBD","Azamat Jumabayev", 20, "azamatjumabayev8774@gmail.com")
 user2 = User("juma_developer","Aziz Jumabayev", 21, "azizbek@gmail.com")
 user3 = User("Onedeveloper","Afzalbek Jumabayev", 23, "onedevelopernaik@gmail.com")
 user1.get_info()
@@ -25,5 +25,3 @@ print(user1.get_age())
 print(user1.get_email())
 user2.get_info()
 user3.get_info()
-
-
