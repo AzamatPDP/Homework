829053c9bc545c6f032e4066e528c73df9784009
Azamat Jumabayev <158017128+AzamatPDP@users.noreply.github.com>
2024-05-09 15:20:23 +0500

    Update Lesson_21.py

--- Lesson_21.py
+++ Lesson_21.py
@@ -32,12 +32,27 @@ import re
 # print(birthDay(2004,3,6))
 
 # 4 - topshiriq
-andoza = "^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
-numMSG = "Telefon raqam kiriting:"
-while True:
-    number = input(numMSG)
-    if re.match(andoza,number):
-        print("To'gri telefon raqam")
-        break
-    else:
-        print("Bu telefon raqam emas")
+# andoza = "^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
+# numMSG = "Telefon raqam kiriting:"
+# while True:
+#     number = input(numMSG)
+#     if re.match(andoza,number):
+#         print("To'gri telefon raqam")
+#         break
+#     else:
+#         print("Bu telefon raqam emas")
+
+
+from datetime import datetime
+def yosh_hisobla(tugilgan_sana):
+    tugilgan_sana = datetime.strptime(tugilgan_sana, '%Y-%m-%d')
+    hozirgi_sana = datetime.now()
+    farq = hozirgi_sana - tugilgan_sana
+    kunlar = farq.days
+    yillar = kunlar // 365
+    oy = (kunlar % 365) // 30
+    kun = (kunlar % 365) % 30
+    return f"Siz {yillar} yil, {oy} oy va {kun} kun yashadingiz."
+
+# Foydalanish misoli:
+print(yosh_hisobla('2004-08-06'))
